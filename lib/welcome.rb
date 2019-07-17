48424dc6b4c7422ba695446a470e10eb6579e8b6
Daisy King <daisyclarefaithking@gmail.com>
2019-07-17 14:44:55 +0000

    Automatically backed up by Learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,3 +1,5 @@
+puts "Welome to Tic Tac Toe!"
 # Edit this file to output "Welcome to Tic Tac Toe!"
-# You can see what this file does by running:
+# You can see what thics file does by running:
 # ruby lib/welcome.rb from your terminal.
+ 
